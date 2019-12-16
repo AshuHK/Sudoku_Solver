367daba092a3deb0eb97271a64f1796d88b4d39a
Ashu Kher <ashukher1@gmail.com>
2019-12-16 18:30:12 -0500

    added solve_board and valid

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 # for testing purposes 
 
+def solve_board(board): 
+
+  first_empty_tuple = find_empty(board)
+  if first_empty_tuple != (-1, -1): 
+     return True
+  else: 
+    row, col = first_empty_tuple 
+    -
+  pass
+
 def find_empty(board):
   """
   Finds first empty cell on the board
@@ -7,12 +17,33 @@ def find_empty(board):
   Return: tuple of two ints formmated (row, column)
   """
   for row in board: 
-    for cell in row: 
-      if cell == 0: 
-        return (board.index(row), row.index(cell))
+    for spot in row: 
+      if spot == 0: 
+        return (board.index(row), row.index(spot))
   
   return (-1, -1)
 
+def valid(board, num_int, pos_tuple): 
+  # row check
+  for i in range(len(board[0])): 
+    if board[pos_tuple[0]][i] == num and pos_tuple[1] != i: 
+      return False 
+
+  # column check 
+  for i in range(len(board)): 
+    if board[i][pos_tuple[1]] == num and pos_tuple[0] != i: 
+      return False 
+  
+  # cell check
+  cell_x = pos_tuple[1] // 3  # column
+  cell_y = pos_tuple[0] // 3  # row 
+
+  for i in range(cell_y * 3, cell_y * 3 + 3): 
+    for j in range(cell_x * 3, cell_x * 3 + 3): 
+      if board[i][j] == num and (i,j) != pos_tuple: 
+        return False
+
+  return True
 
 def print_board(board):
   """
@@ -47,6 +78,9 @@ def main():
          [0,0,5,2,0,6,3,0,0]] 
   print(" ----Original Board----") 
   print_board(board) 
+
+  print("\n ----Solved Board----")
+  print_board(board)
   # print(find_empty(board))
 
   pass
