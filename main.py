d08af6c0d5595fbb107f263f65edb90ce7b109de
Ashu Kher <ashukher1@gmail.com>
2019-12-19 14:31:35 -0500

    testing new GUI

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-# import pygame
-# TODO: need to find a better GUI to work with 
-import sys
-# import solver 
+# # import pygame
+# # TODO: need to find a better GUI to work with 
+# import sys
+# # import solver 
 
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+TestApp().run()
