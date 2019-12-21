b75e47934ff39d801d564da404d03ccfa814eeab
Ashu Kher <ashukher1@gmail.com>
2019-12-21 02:40:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time 
+# import solver
 from kivy.app import App
 from kivy.core.window import Window 
 from kivy.uix.button import Button
@@ -23,20 +23,19 @@ class SudokuWindow(App):
                     app_layout.add_widget(Button(text = str(num)))
                 else: 
                     app_layout.add_widget(Button(text = ""))
-        
         return app_layout
 
 
-    def solve_board(self, app_layout, board): 
-        return False
+    # def solve_board(self, app_layout, board): 
+    #     return False
         
     
-    def find_empty(self, board): 
-        return (-1, -1)
+    # def find_empty(self, board): 
+    #     return (-1, -1)
     
 
-    def valid(self, board, num_int, pos_tuple): 
-        return True
+    # def valid(self, board, num_int, pos_tuple): 
+    #     return True
 
 
 if __name__ == "__main__": 
