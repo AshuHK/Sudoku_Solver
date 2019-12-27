111eb8f6914e1c3ad1006564df8d9990f38fd49e
Ashu Kher <ashukher1@gmail.com>
2019-12-26 22:45:48 -0500

    trimmed trailiing whitespace

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import time 
+import time
 
-def solve_board(board): 
+def solve_board(board):
   """
   Solves the board and checks if it is solved
   Parameters: board (2-D list of ints)
-  Returns: boolean for if the board is solved 
+  Returns: boolean for if the board is solved
   """
   first_empty_tuple = find_empty(board)
-  if first_empty_tuple == (-1, -1): 
+  if first_empty_tuple == (-1, -1):
      return True
-  else: 
+  else:
     row_int, col_int = first_empty_tuple
-  
-  for i in range(1, 10): 
-    if valid(board, i, (row_int, col_int)): 
-      board[row_int][col_int] = i 
+
+  for i in range(1, 10):
+    if valid(board, i, (row_int, col_int)):
+      board[row_int][col_int] = i
 
       if solve_board(board):
-        return True 
-      
-      board[row_int][col_int] = 0 
+        return True
+
+      board[row_int][col_int] = 0
 
   return False
 
@@ -30,37 +30,37 @@ def find_empty(board):
   Parameters: board (2-D list of ints)
   Return: tuple of two ints formmated (row, column)
   """
-  for row in board: 
-    for spot in row: 
-      if spot == 0: 
+  for row in board:
+    for spot in row:
+      if spot == 0:
         return (board.index(row), row.index(spot))
-  
+
   return (-1, -1)
 
-def valid(board, num_int, pos_tuple): 
+def valid(board, num_int, pos_tuple):
   """
-  Checks if the number is valide at a certain position 
-  Parameters: board (2-D list of ints), num_int (int to insert), 
-              pos_tuple [(row, col) pair of ints] 
+  Checks if the number is valide at a certain position
+  Parameters: board (2-D list of ints), num_int (int to insert),
+              pos_tuple [(row, col) pair of ints]
   Return: boolean of if the number is valid at that position
   """
   # row check
-  for i in range(len(board[0])): 
-    if board[pos_tuple[0]][i] == num_int and pos_tuple[1] != i: 
-      return False 
+  for i in range(len(board[0])):
+    if board[pos_tuple[0]][i] == num_int and pos_tuple[1] != i:
+      return False
 
-  # column check 
-  for i in range(len(board)): 
-    if board[i][pos_tuple[1]] == num_int and pos_tuple[0] != i: 
-      return False 
-  
+  # column check
+  for i in range(len(board)):
+    if board[i][pos_tuple[1]] == num_int and pos_tuple[0] != i:
+      return False
+
   # cell check
   cell_x = pos_tuple[1] // 3  # column
-  cell_y = pos_tuple[0] // 3  # row 
+  cell_y = pos_tuple[0] // 3  # row
 
-  for i in range(cell_y * 3, cell_y * 3 + 3): 
-    for j in range(cell_x * 3, cell_x * 3 + 3): 
-      if board[i][j] == num_int and (i,j) != pos_tuple: 
+  for i in range(cell_y * 3, cell_y * 3 + 3):
+    for j in range(cell_x * 3, cell_x * 3 + 3):
+      if board[i][j] == num_int and (i,j) != pos_tuple:
         return False
 
   return True
@@ -70,45 +70,45 @@ def print_board(board):
   Prints the board
   Parameters: board (2-D list of ints)
   Return: None
-  """ 
+  """
   print("-----------------------")
-  for i in range(len(board)): 
-    if i % 3 == 0 and i != 0: 
+  for i in range(len(board)):
+    if i % 3 == 0 and i != 0:
       print("-----------------------")
 
-    for j in range(len(board[0])): 
-      if j % 3 == 0 and j != 0: 
+    for j in range(len(board[0])):
+      if j % 3 == 0 and j != 0:
         print(" | ", end = "")
-      
-      if j == 8: 
+
+      if j == 8:
         print(board[i][j], end = "\n")
-      else: 
+      else:
         print(str(board[i][j]) + " ", end = "")
   pass
 
 def main():
-  board=[[8,0,0,0,0,0,0,0,0], 
-         [0,0,3,6,0,0,0,0,0], 
-         [0,7,0,0,9,0,2,0,0], 
-         [0,5,0,0,0,7,0,0,0], 
-         [0,0,0,0,4,5,7,0,0], 
-         [0,0,0,1,0,0,0,3,0], 
-         [0,0,1,0,0,0,0,6,8], 
-         [0,0,8,5,0,0,0,1,0], 
-         [0,9,0,0,0,0,4,0,0]] 
+  board=[[8,0,0,0,0,0,0,0,0],
+         [0,0,3,6,0,0,0,0,0],
+         [0,7,0,0,9,0,2,0,0],
+         [0,5,0,0,0,7,0,0,0],
+         [0,0,0,0,4,5,7,0,0],
+         [0,0,0,1,0,0,0,3,0],
+         [0,0,1,0,0,0,0,6,8],
+         [0,0,8,5,0,0,0,1,0],
+         [0,9,0,0,0,0,4,0,0]]
 
-  print(" ----Original Board----") 
-  print_board(board) 
+  print(" ----Original Board----")
+  print_board(board)
 
   start_time = time.time()
-  solve_board(board) 
+  solve_board(board)
 
   print("\n ----Solved Board----")
   print_board(board)
-  
+
   print("Solve time: {} seconds".format(time.time() - start_time))
   return None
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
   main()
